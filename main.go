f54f035fad083014cb7a70bd01e0a84d2559a236
asitsonawane <sonawaneasit@gmail.com>
2023-10-31 01:10:38 +0530

    Updated Readme

--- main.go
+++ main.go
@@ -11,13 +11,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Movie represents a movie entity.
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
-	Title    string    ` json:"title"`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
+// Director represents the director of a movie.
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
@@ -25,11 +27,13 @@ type Director struct {
 
 var movies []Movie
 
+// getMovies handles the GET request to retrieve all movies.
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 }
 
+// deleteMovie handles the DELETE request to delete a specific movie by ID.
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -42,6 +46,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
+// getMovie handles the GET request to retrieve a specific movie by ID.
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -53,6 +58,7 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// createMovie handles the POST request to create a new movie.
 func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
@@ -62,6 +68,7 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+// updateMovie handles the PUT request to update an existing movie by ID.
 func updateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -82,10 +89,13 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
+	// Sample data for demonstration.
 	movies = append(movies, Movie{ID: "1", Isbn: "438277", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "45455", Title: "Movie Two", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
+
+	// Define the routes and handlers.
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
